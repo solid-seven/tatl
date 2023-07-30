ef80844b9bd772c0f0dad20c5ce65352c7493a45
Abe Melvin <abe.melvin@gamil.com>
2023-07-30 16:23:42 -0400

    use subprocess stdout arg to write to file

--- .github/scripts/execute_ebpf.py
+++ .github/scripts/execute_ebpf.py
@@ -8,12 +8,13 @@ def execute_ebpf() -> bool:
     Create a subprocess to invoke bootstrap, and time it out after 10s
     """
     try:
-        results = subprocess.run(
-            args=["./bootstrap > $GITHUB_WORKSPACE/ebpf-output.log"],
-            shell=True,
-            check=False,
-            timeout=10
-        )
+        with open("$GITHUB_WORKSPACE/ebpf-output.log", encoding='utf-8') as output_file:
+            results = subprocess.run(
+                args=["./bootstrap"],
+                stdout=output_file,
+                check=False,
+                timeout=10
+            )
     except subprocess.TimeoutExpired:
         return True
     if results.returncode == 0:
