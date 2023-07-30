92d11dd87e2ecb498670e4d1f27a8a5dd9ccc78f
Abe Melvin <abe.melvin@gamil.com>
2023-07-30 17:04:10 -0400

    specify output file location as argument

--- .github/scripts/execute_ebpf.py
+++ .github/scripts/execute_ebpf.py
@@ -1,17 +1,28 @@
 """Modules"""
+import argparse
 import subprocess
 import sys
 
 
-def execute_ebpf() -> bool:
+def parse_args() -> str:
+    """
+    Get the requested output file path
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output_file')
+    args = parser.parse_args()
+    return str(args.output_file)
+
+
+def execute_ebpf(output_file: str) -> bool:
     """
     Create a subprocess to invoke bootstrap, and time it out after 10s
     """
-    output_file = open('ebpf-output.txt', mode='a', encoding='utf-8')
+    output_fp = open(output_file, mode='a', encoding='utf-8')
     try:
         results = subprocess.run(
             args=["./bootstrap"],
-            stdout=output_file,
+            stdout=output_fp,
             stderr=subprocess.STDOUT,
             shell=True,
             check=False,
@@ -19,15 +30,16 @@ def execute_ebpf() -> bool:
             timeout=10
         )
     except subprocess.TimeoutExpired:
-        output_file.close()
+        output_fp.close()
         return True
-    output_file.close()
+    output_fp.close()
     if results.returncode == 0:
         return True
     return False
 
 
 if __name__ == "__main__":
-    if execute_ebpf():
+    REQUESTED_OUTPUT_FILE = parse_args()
+    if execute_ebpf(REQUESTED_OUTPUT_FILE):
         sys.exit(0)
     sys.exit(1)
