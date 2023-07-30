4bccf7184089527cd757300f3426b8902095cd61
Abe Melvin <abe.melvin@gamil.com>
2023-07-30 15:53:23 -0400

    python linting fixes

---
+++ .github/scripts/execute_ebpf.py
@@ -0,0 +1,29 @@
+"""Modules"""
+import subprocess
+import sys
+
+
+def execute_ebpf() -> bool:
+    """
+    Create a subprocess to invoke bootstrap, and time it out after 10s
+    """
+    results = subprocess.run(
+        args=[
+            "sudo",
+            "timeout",
+            "10s",
+            "$GITHUB_WORKSPACE/libbpf-bootstrap/examples/c/bootstrap " \
+            "> $GITHUB_WORKSPACE/ebpf-output.log",
+        ],
+        shell=True,
+        check=False,
+    )
+    if results.returncode in [0, 124]:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    if execute_ebpf():
+        sys.exit(0)
+    sys.exit(1)
