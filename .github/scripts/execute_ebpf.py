73148b5069855e1b8b65f9593e2b7fb4a1e4615d
Abe Melvin <abe.melvin@gamil.com>
2023-07-30 16:27:35 -0400

    specify write mode for open call

--- .github/scripts/execute_ebpf.py
+++ .github/scripts/execute_ebpf.py
@@ -8,7 +8,7 @@ def execute_ebpf() -> bool:
     Create a subprocess to invoke bootstrap, and time it out after 10s
     """
     try:
-        with open("$GITHUB_WORKSPACE/ebpf-output.log", encoding='utf-8') as output_file:
+        with open("$GITHUB_WORKSPACE/ebpf-output.log", mode='w', encoding='utf-8') as output_file:
             results = subprocess.run(
                 args=["./bootstrap"],
                 stdout=output_file,
