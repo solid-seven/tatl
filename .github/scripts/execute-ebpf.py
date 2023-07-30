6eb662faa40bd2422fc5afe38d2afe1806ae4339
Abe Melvin <abe.melvin@gamil.com>
2023-07-30 15:41:26 -0400

    start tracking scripts dir

---
+++ .github/scripts/execute-ebpf.py
@@ -0,0 +1,23 @@
+import subprocess
+import sys
+
+
+def execute_ebpf() -> bool:
+    results = subprocess.run(
+        args=[
+            "timeout",
+            "10s",
+            "$GITHUB_WORKSPACE/libbpf-bootstrap/examples/c/bootstrap",
+            ">",
+            "$GITHUB_WORKSPACE/ebpf-output.log"
+        ],
+        shell=True
+    )
+    if results.returncode in [0, 124]:
+        return True
+    return False
+
+if __name__ == '__main__':
+    if execute_ebpf():
+        sys.exit(0)
+    sys.exit(1)
