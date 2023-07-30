59f59ec178efbdc6afdd5952f2f0d55482048079
Abe Melvin <abe.melvin@gamil.com>
2023-07-30 16:45:13 -0400

    change run args

--- .github/scripts/execute_ebpf.py
+++ .github/scripts/execute_ebpf.py
@@ -8,11 +8,14 @@ def execute_ebpf() -> bool:
     Create a subprocess to invoke bootstrap, and time it out after 10s
     """
     try:
-        with open('ebpf-output.log', mode='w', encoding='utf-8') as output_file:
+        with open('ebpf-output.txt', mode='w', encoding='utf-8') as output_file:
             results = subprocess.run(
                 args=["./bootstrap"],
                 stdout=output_file,
+                stderr=subprocess.STDOUT,
+                shell=True,
                 check=False,
+                text=True,
                 timeout=10
             )
     except subprocess.TimeoutExpired:
