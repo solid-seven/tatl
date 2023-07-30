92d67042c9d329ce5de1ac75213d325cdef37668
Abe Melvin <abe.melvin@gamil.com>
2023-07-30 16:52:55 -0400

    change file handling

--- .github/scripts/execute_ebpf.py
+++ .github/scripts/execute_ebpf.py
@@ -7,19 +7,21 @@ def execute_ebpf() -> bool:
     """
     Create a subprocess to invoke bootstrap, and time it out after 10s
     """
+    output_file = open('ebpf-output.txt', mode='a', encoding='utf-8')
     try:
-        with open('ebpf-output.txt', mode='w', encoding='utf-8') as output_file:
-            results = subprocess.run(
-                args=["./bootstrap"],
-                stdout=output_file,
-                stderr=subprocess.STDOUT,
-                shell=True,
-                check=False,
-                text=True,
-                timeout=10
-            )
+        results = subprocess.run(
+            args=["./bootstrap"],
+            stdout=output_file,
+            stderr=subprocess.STDOUT,
+            shell=True,
+            check=False,
+            text=True,
+            timeout=10
+        )
     except subprocess.TimeoutExpired:
+        output_file.close()
         return True
+    output_file.close()
     if results.returncode == 0:
         return True
     return False
