92f89a29393004756e83f59cea6456fbeff0d84a
Abe Melvin <abe.melvin@gamil.com>
2023-07-30 17:40:26 -0400

    add whoami command to generate logs

--- .github/scripts/execute_ebpf.py
+++ .github/scripts/execute_ebpf.py
@@ -2,6 +2,7 @@
 import argparse
 import subprocess
 import sys
+import time
 
 
 def parse_args() -> str:
@@ -19,18 +20,18 @@ def execute_ebpf(output_file: str) -> bool:
     Create a subprocess to invoke bootstrap, and time it out after 10s
     """
     try:
-        results = subprocess.run(
+        bootstrap = subprocess.Popen(
             args=[f"./bootstrap > {output_file}"],
             shell=True,
-            check=False,
             text=True,
-            timeout=10
         )
-    except subprocess.TimeoutExpired:
-        return True
-    if results.returncode == 0:
-        return True
-    return False
+        time.sleep(1)
+        subprocess.run(args=['whoami'], check=True)
+        time.sleep(1)
+        bootstrap.terminate()
+    except Exception: # pylint: disable=broad-exception-caught
+        return False
+    return True
 
 
 if __name__ == "__main__":
